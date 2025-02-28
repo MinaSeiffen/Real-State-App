3d9946586790a1e0c5b60859625a0d42b04ade8a
MinaSeiffen <minaseiffen@gmail.com>
2025-02-28 16:42:55 +0200

    Preparing Frontend app for the real state app

---
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from "@/app/(nondashboard)/landing/HeroSection";
+import FeaturesSection from "@/app/(nondashboard)/landing/FeaturesSection";
+
+function Landing() {
+    return (
+        <div>
+            <HeroSection/>
+            <FeaturesSection/>
+        </div>
+    );
+}
+
+export default Landing;
